24ef1c85da98e873b58b664db8a8c1615ed62e63
Yasser Boaventura <yasserboaventura78@gmail.com>
2026-01-12 02:29:22 +0000

    --fixed

--- src/main/java/com/ms/user/Entity/UserModel.java
+++ src/main/java/com/ms/user/Entity/UserModel.java
@@ -21,12 +21,12 @@ import lombok.Setter;
 @RequiredArgsConstructor
 public class UserModel  implements Serializable{
 	 
-   private static final long serialVersion = 1L;
-	 @Id
+   private static final long serialVersionUID = 1L;
+	 @Id 
 	 @GeneratedValue(strategy = GenerationType.AUTO)
 	 private UUID userId;
 	 private String email;
-     private String nome;
+     private String nome; 
 	
 } 
  
